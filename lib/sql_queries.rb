e3193282550522709c8eb3b0e01341fbc8f44d85
Emily Morgan <emorgan05@gmail.com>
2017-10-22 18:05:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, SUM(pledges.amount) FROM users
-INNER JOIN pledges on pledges.user_id = users.id
+INNER JOIN pledges ON pledges.user_id = users.id
 GROUP BY users.id
 ORDER BY users.name ASC;"
 end
