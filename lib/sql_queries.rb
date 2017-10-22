c18a33f675bd9ddfc268bba96acc4d962f6099cb
Emily Morgan <emorgan05@gmail.com>
2017-10-22 18:22:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ ORDER BY users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount) AS amount_over_goal FROM projects
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_over_goal FROM projects
 INNER JOIN pledges ON pledges.project_id = projects.id
 WHERE pledges.amount >= projects.funding_goal;"
 end
